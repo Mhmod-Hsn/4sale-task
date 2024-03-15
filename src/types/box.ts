eb269f6b84de2f7f9dc42819d40d5b0db91c1f42
HASSAN Mahmoud <Mahmoud.HASSAN@cegedim.com>
2024-03-15 03:04:27 +0200

    cleanings

--- src/types/box.ts
+++
@@ -1,9 +0,0 @@
-import { TClass } from './class';
-import { TPoint } from './point';
-
-export type TBox = {
-	id?: string;
-	points: TPoint;
-	text: string;
-	class: TClass | string;
-};
--- src/types/file.ts
+++
@@ -1,8 +0,0 @@
-import { TBox } from './box';
-
-export type TFile = {
-	base64: string;
-	boxes: TBox[];
-	_id: string;
-	name: string;
-};
